8cc1580a0c39ce38bb5f2254ae90583383bc26be
LucasRaggio <69179808+LucasRaggio@users.noreply.github.com>
2020-08-05 22:24:35 -0400

    Create Hola.py
    Este en el código basico que simplemente imprime cosas.

---
+++ Hola.py
@@ -0,0 +1,9 @@
+Nombre = 'Lucas Raggio Barbieri' 
+Edad = int(22)
+Sexo = 'Masculino.' 
+Fecha = ' 7 de agosto'   
+
+#Mostrando la informacion 
+
+print ('Hola, mi nombre es ', Nombre , ' tengo ' , Edad , 'years y mi sexo es ', Sexo)
+
