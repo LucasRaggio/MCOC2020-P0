b193bae2e94d549c8278ca6c97792035c099cc3f
LucasRaggio <69179808+LucasRaggio@users.noreply.github.com>
2020-08-05 23:16:18 -0400

    Update Hola.py

--- Hola.py
+++ Hola.py
@@ -1,9 +1,20 @@
+
 Nombre = 'Lucas Raggio Barbieri' 
 Edad = int(22)
 Sexo = 'Masculino.' 
-Fecha = ' 7 de agosto'   
+Fecha = ' 7 de agosto' 
 
 #Mostrando la informacion 
 
-print ('Hola, mi nombre es ', Nombre , ' tengo ' , Edad , 'years y mi sexo es ', Sexo)
+print ('forma 1')
+print ('Hola, mi nombre es', Nombre , 'tengo' , Edad , 'years y mi sexo es', Sexo)
 
+print ('forma 2')
+print ('Hola, mi nombre es Lucas Raggio Barbieri tengo 22 years y mi sexo es Masculino.')
+
+
+print ('forma 3')
+print ('Hola, mi nombre es ' + Nombre + ' tengo ' + str(Edad) + ' years y mi sexo es ' +  Sexo)
+
+N = 5 
+print ('Hola ' * N )
